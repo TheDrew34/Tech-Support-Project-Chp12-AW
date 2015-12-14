6f3a2bcf53ee8f3201b16220d3cd26bed3391568
unknown <G00230869@W213-27.academic.raritanval.edu>
2015-12-14 18:23:16 -0500

    Exercise 12.47 - Read in Reponse Mappings
    Kept the accounting for empty lines, assumes pairings of key and value

---
+++ __SHELL15.java
@@ -0,0 +1,10 @@
+
+public class __SHELL15 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\G00230869\\Desktop\\GitHub\\Tech-Support-Project-Chp12-AW");
+final SupportSystem supportS1 = (SupportSystem)__bluej_runtime_scope.get("supportS1");
+
+
+supportS1.start();
+
+}}
